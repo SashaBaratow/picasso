d8323c81debea4510198434cad9eb9c29c682ce3
Sasha <sashabaratow@gmail.com>
2026-03-24 19:52:25 +0600

    init project

---
+++ picasso-crm-api/src/main/java/com/picasso/crm/api/PicassoCrmApiApplication.java
@@ -0,0 +1,13 @@
+package com.picasso.crm.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicassoCrmApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicassoCrmApiApplication.class, args);
+    }
+
+}
---
+++ picasso-crm-api/src/test/java/com/picasso/crm/api/PicassoCrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picasso.crm.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicassoCrmApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
